cfb632398a3293c8b4637e725cf33d0caabdb98c
Razel Tech <razeltech.in@gmail.com>
2026-07-21 14:38:17 +0530

    Optimize v2: Add beautiful, collapsible Steps Instructions & Tips Cards to Steps 1, 2, 3, and 4 in booth.html; added dashed instructions accordion CSS; SW v3.9

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Razel Tech Photo Booth Pro - Service Worker
  * Enables offline capability and PWA standalone app execution
  */
-const CACHE_NAME = 'digismile-studio-v3.8';
+const CACHE_NAME = 'digismile-studio-v3.9';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
